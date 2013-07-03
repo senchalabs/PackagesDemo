fdd4503443911e8299a4d37c651303a1f76396ef
kkrohe <kkrohe@gmail.com>
2013-07-02 23:51:54 -0500

    add customizations to SharedPackages

---
+++ Workspace2/packages/SharedPackageWS2/src-ext/CustomButton.js
@@ -0,0 +1,14 @@
+Ext.define("SharedPackageWS2.CustomButton", {
+    extend: 'Ext.button.Button',
+    xtype: 'customButtonSharedPackage',
+    requires: [
+        'Ext.window.MessageBox',
+        'SharedPackageWS2.SharedClass'
+    ],
+    text: 'Custom Button from SharedPackageWS2',
+    width: 400,
+    handler: function() {
+        var shared = new SharedPackageWS2.SharedClass();
+        Ext.Msg.alert("Button Clicked", shared.getSharedText(), Ext.emptyFn);
+    }
+});
---
+++ Workspace2/packages/SharedPackageWS2/src/SharedClass.js
@@ -0,0 +1,7 @@
+Ext.define("SharedPackageWS2.SharedClass", {
+
+    getSharedText: function() {
+        return "This text is from SharedPackageWS2.SharedClass";
+    }
+    
+});
