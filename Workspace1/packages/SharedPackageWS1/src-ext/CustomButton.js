81e80a0ebad1652e5a12da75c235723adab2220e
kkrohe <kkrohe@gmail.com>
2013-07-02 19:26:24 -0500

    update SharedPackage with ext specific code

---
+++ Workspace1/packages/SharedPackageWS1/src-ext/CustomButton.js
@@ -0,0 +1,14 @@
+Ext.define("SharedPackageWS1.CustomButton", {
+    extend: 'Ext.button.Button',
+    xtype: 'customButtonSharedPackage',
+    requires: [
+        'Ext.window.MessageBox',
+        'SharedPackageWS1.SharedClass'
+    ],
+    text: 'Custom Button from SharedPackageWS1',
+    width: 400,
+    handler: function() {
+        var shared = new SharedPackageWS1.SharedClass();
+        Ext.Msg.alert("Button Clicked", shared.getSharedText(), Ext.emptyFn);
+    }
+});
